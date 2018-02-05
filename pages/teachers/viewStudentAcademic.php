a77e1e2a239659189bf9cc42374942ac4b599216
nanaadjoabentil <nana.bentil@outlook.com>
2018-02-05 22:23:05 +0000

    corrected js

--- pages/teachers/viewStudentAcademic.php
+++ pages/teachers/viewStudentAcademic.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link type="text/css" rel="stylesheet" href="../../css/register.css"  media="screen,projection"/>
     <script type="text/javascript" src="../js/bootstrap.js"></script>
-    <script type="text/javascript" src="../js/bootstrap.min,js"></script>
+    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
