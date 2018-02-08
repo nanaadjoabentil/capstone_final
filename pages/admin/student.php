ac713bd5d57d365cc0b0c08da1880c28763faf7c
nanaadjoabentil <nana.bentil@outlook.com>
2018-02-08 14:57:02 +0000

    feb 8 2:56pm

--- pages/admin/student.php
+++ pages/admin/student.php
@@ -8,8 +8,8 @@
   <nav>
     <ul>
       <a href="registerStudentPersonal.php"><button id="dbutton"><img src="../../images/plus.png"><br><br>Add a New Student</button></a>
-      <a href="registerStudentHealth.php"><button id="dbutton"><img src="../../images/plus.png">Add Student's Health Information</button></a>
-      <a href="registerStudentFinancial.php"><button id="dbutton"><img src="../../images/plus.png">Add Student Financial Information</button></a>
+      <a href="registerStudentHealth.php"><button id="dbutton"><img src="../../images/plus.png"><br><br>Add Student's Health Information</button></a>
+      <a href="registerStudentFinancial.php"><button id="dbutton"><img src="../../images/plus.png"><br><br>Add Student Financial Information</button></a>
       <a href="../teachers/viewStudentAcademic.php"><button id="dbutton">View Student Academic Information</button></a>
       <a href="viewStudentHealth.php"><button id="dbutton">View Student Health Information</button></a>
       <a href="viewStudentFinancial.php"><button id="dbutton">View Student Financial Information</button></a>
